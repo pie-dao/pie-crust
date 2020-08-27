8e2f4a7a8c84d182e723880b60a5378a8c90043b
Mick de Graaf <mick@mickdegraaf.nl>
2020-08-27 19:22:16 +0200

    crust deployment task

--- buidler.config.ts
+++ buidler.config.ts
@@ -1,4 +1,7 @@
-import { usePlugin } from  "@nomiclabs/buidler/config";
+import { usePlugin, task, types } from  "@nomiclabs/buidler/config";
+import { ArgumentType } from "@nomiclabs/buidler/internal/core/params/argumentTypes";
+import { Crust } from "./typechain/Crust";
+import { CrustFactory } from "./typechain";
 
 usePlugin("@nomiclabs/buidler-ethers");
 
@@ -12,6 +15,9 @@ const config = {
     localhost: {
       url: 'http://localhost:8545'
     },
+    frame: {
+      url: "http://localhost:1248"
+    }
   },
   solc: {
     version: '0.4.24',
@@ -21,5 +27,16 @@ const config = {
     }
   },
 }
+
+task("deploy-crust", "Deploys the Crust contract")
+  .addParam("token0", "token0", null, types.string)
+  .addParam("token1", "token1", null, types.string)
+  .setAction(async (taskArgs, {ethers}) => {
+    const signers = await ethers.getSigners();
+    const crust: Crust = await (new CrustFactory(signers[0])).deploy([taskArgs.token0, taskArgs.token1]);
+    console.log(`Deployed crust at: ${crust.address}`);
+
+    return crust;
+});
 
 export default config;
